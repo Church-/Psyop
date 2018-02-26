49c8dc5534a580481ec83d90396dbd252de05760
Noah Hendrickson <Church-@users.noreply.github.com>
2018-02-26 03:32:10 -0500

    Create run.rs

---
+++ src/run.rs
@@ -0,0 +1,98 @@
+extern crate regex;
+mod process;
+use std::str::Lines;
+use regex::Regex;
+use std::path::Path;
+use std::collections::BTreeMap;
+use std::fs::File;
+use process::Process;
+
+
+fn readFile(filePath: String)  -> Lines {
+	let tmpFile = File::open(filePath);
+	let contents = String::new();
+	tmpFile.read_to_string(&mut contents);
+	//let mut lines: Lines = contents.lines()
+	//Lines
+	contents.lines()
+}
+
+fn splitOnce(in_string: &str, pattern: &str) -> (&'static str, &'static str) {
+    let mut splitter = in_string.splitn(2, pattern);
+    let first = splitter.next().unwrap();
+    let second = splitter.next().unwrap();
+    (first, second)
+}
+
+
+fn assignProcessStats(buffer: Lines) -> (isize,Process) {
+	let tmpVec: Vec<&str> = Vec::new();
+	for i in buffer {
+		let (first,second) = splitOnce(i, ":"); 
+		match first {
+			"name" => tmpVec.push(second),
+			"state" => tmpVec.push(second),
+			"tgid" => tmpVec.push(second),
+			"VmRSS" => tmpVec.push(second),
+			"" => tmpVec.push(second),
+			_ => continue
+		}
+	}
+		
+}
+
+fn getSystemMemory() {
+	let meminfo = readFile("/proc/meminfo");
+	let systemMem = String::new();
+		for i in meminfo {
+			let (first,second) = splitOnce(i, ":"); 
+			match first {
+				"name" => (systemMem,) = splitOnce(first, "kB") 
+				_ => continue
+			}
+		}
+	systemMem.parse::<isize>()
+}
+
+
+fn printStatusMap(processMap: BTreeMap<isize,Process>) {
+	let tmpString = String::new();
+	for (_,processes) in processMap {
+		tmpString.push(processes::toString());
+		tmpString.push("\n");
+	}
+	println!("{}",tmpString);
+}
+
+fn parseProce() -> Vec<String> {}
+
+fn run() {
+	let mut processMap: BTreeMap<isize,Process> = BTreeMap::new();
+	let path = Path::new("/proc");
+	let mut folders: Vec<String> = Vec::new();
+	let re = Regex::new(r"^\d+$");
+	let mut tmpPath = "";
+	for entry in path.read_dir().expect("Failed to read folder contents.") {
+		if let Ok(entry) = entry {
+		        tmpPath = entry.path().to_str();
+		    }
+		match re.is_match(tmpPath) {
+			true => folders.push(tmpPath),
+			false => continue,
+		}
+	}
+
+	for i in folders {
+		let mut mutPath = path.to_path_buf();
+		mutPath.push(i);
+		match mutPath.is_dir() {
+			true => {
+			    mutPath.push("/status");
+		    	let mut buffer = readFile(mutPath.to_str().unwrap());
+				let mut process = assignProcessStats(buffer);
+				processMap.insert(i,process);
+		        }
+		    false => continue,
+		}
+	}
+}
